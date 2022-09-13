f7f1272bff82b6fd8a91fe0c5a826e3863ce112a
Caio Dias <caiocozendey@hotmail.com>
2022-09-13 20:09:39 -0300

    Implementando navegação de telas e toolbar

--- app/src/main/java/com/example/yugiohdeckgenerator/GenerateDeckActivity.kt
+++ app/src/main/java/com/example/yugiohdeckgenerator/GenerateDeckActivity.kt
@@ -2,10 +2,19 @@ package com.example.yugiohdeckgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class GenerateDeckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_deck)
+
+        setSupportActionBar(findViewById(R.id.toolbarGenerate))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.generate_deck_option, menu)
+        return true
     }
 }
--- app/src/main/java/com/example/yugiohdeckgenerator/MainActivity.kt
+++ app/src/main/java/com/example/yugiohdeckgenerator/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.yugiohdeckgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val generateDeck = findViewById<Button>(R.id.generateDeckBtn)
+        val allCards = findViewById<Button>(R.id.allCardsBtn)
+        val myDecks = findViewById<Button>(R.id.myDecksBtn)
+
+        generateDeck.setOnClickListener {
+            val intent = Intent(this, GenerateDeckActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
