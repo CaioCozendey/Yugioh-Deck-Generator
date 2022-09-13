ee596c528bce5198fc8eb7b87385381db7c9c2d3
Caio Dias <caiocozendey@hotmail.com>
2022-09-13 19:30:22 -0300

    Layout tela principal pronta

---
+++ app/src/main/java/com/example/yugiohdeckgenerator/GenerateDeckActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yugiohdeckgenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenerateDeckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_deck)
+    }
+}
